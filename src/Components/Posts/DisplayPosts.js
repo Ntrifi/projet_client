9c1e2be202667298cbde8b07b9df6bf4607c8f98
Ntrifi <nouhatrifi94@gmail.com>
2024-05-14 20:16:51 +0200

    secondclient

--- src/Components/Posts/DisplayPosts.js
+++ src/Components/Posts/DisplayPosts.js
@@ -5,7 +5,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { deletePost, getAllPosts } from "../../Redux/actions/post";
 import "./style.css";
 import { Link, useNavigate } from "react-router-dom";
+
 function DisplayPosts () {
+
+    const isAuth = useSelector((state) => state.userReducer.isAuth);
     const dispatch = useDispatch();
     const navigate = useNavigate()
     useEffect(() => {
@@ -18,21 +21,34 @@ function DisplayPosts () {
     }
     return (
         <div className="pub-card">
-            {posts && posts.map((p) =>
-                <Card className="card" style={{ width: '18rem' }}>
-                    <Card.Img style ={{ height:'170px'}} variant="top" src={p.LinkImage} />
-                    <Card.Body>
-                        <Card.Title>{p.Title}</Card.Title>
-                        <Card.Text>
-                            {p.Description}
-                        </Card.Text>
-                        
-                        <Button variant="outline-secondary" onClick={()=>handleDelete(p._id)}>delete</Button>
-                        
-                        <Button variant="outline-secondary">Update</Button>
-                    </Card.Body>
-                </Card>)}
-        </div>
+        {posts &&
+          posts.map((p) => (
+            <Card className="card" style={{ width: "18rem" }} key={p._id}>
+              <Card.Img
+                style={{ height: "170px" }}
+                variant="top"
+                src={p.LinkImage}
+              />
+              <Card.Body>
+                <Card.Title>{p.Title}</Card.Title>
+                <Card.Text>{p.Description}</Card.Text>
+                {isAuth && (
+                  <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                    <Button
+                      variant="outline-secondary"
+                      onClick={() => handleDelete(p._id)}
+                    >
+                      Delete
+                    </Button>
+                    <Button variant="outline-secondary">
+                      Update
+                    </Button>
+                  </div>
+                )}
+              </Card.Body>
+            </Card>
+          ))}
+      </div>
     )
 }
 export default DisplayPosts;
